f7df60d58ea7cfabedeed12f1be01d646fb22bfc
Dominik-Salabura <79972094+Dominik-Salabura@users.noreply.github.com>
2021-11-21 16:06:06 +0100

    Testy i drobne zmiany klas ListServer i MapServer
    Wprowadzone zostały niewielkie zmiany klasach ListServer i MapServer.
    Zostało wykonane po 3 testy dla obu klas

--- servers_skeleton.py
+++ servers_skeleton.py
@@ -5,30 +5,45 @@ from typing import Optional, List, Dict
  
  
 class Product:
-    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
-    
-    def __init__(self, name: str, price: float):
-        if len(name) < 2:
-            raise ValueError
 
+    def __init__(self, name: str, price: float):
+        # Pętla sprawdza warunki dla każdego znaku w nazwie produku
         for letter_iter in range(len(name)):
-            if letter_iter == len(name) - 1:
-                raise ValueError
-            if 65 <= ord(name[letter_iter]) <= 122:  # sprawdza czy występuje co najmniej jedna litera
+            # Jeśli znak jest literą, nic nie rób
+            if 65 <= ord(name[letter_iter]) <= 122:
                 pass
+            # W przeciwnym wypadku...
             else:
-                if 48 <= ord(name[letter_iter]) <= 57:   # jeśli po ciągu liter nastąpi cyfra, tworzy się pętla sprawdzająca ciąg cyfr
+                # ...sprawdź czy nie rozpoczął się już ciąg liczb w nazwie
+                if 48 <= ord(name[letter_iter]) <= 57:
+
+                    # Jeśli nazwa nie zaczyna się od litery, zwracany jest ValueError
                     if letter_iter == 0:
                         raise ValueError
+
+                    # letter_iter określa teraz liczbę liter na początku nazwy
+                    self.n_letters = letter_iter
+
+                    # Pętla sprawdza dla każdego znaku po ciągu liter, czy jest to liczba
                     for number_iter in range(letter_iter, len(name)):
                         if 48 <= ord(name[number_iter]) <= 57:
                             pass
+
+                        # Jeśli któryś znak nie jest liczbą, zwracany jest ValueError
                         else:
                             raise ValueError
+
+                    # Po zakończeniu pomyślnie wewnętrznej pętli kończymy wszelkie iteracje po elementach
                     break
+
                 else:
                     raise ValueError
 
+            # Jeśli w nazwie nie ma żadnych liczb, zwróć ValueError
+            if letter_iter == len(name) - 1:
+                raise ValueError
+
+        # Jeśli cena produktu jest nielogiczna, zwróć ValueError
         if price <= 0:
             raise ValueError
 
@@ -36,13 +51,13 @@ class Product:
         self.price = price
 
     def __eq__(self, other):
-        return self.name == other.name and self.price == other.price  # FIXME: zwróć odpowiednią wartość
+        return self.name == other.name and self.price == other.price
  
     def __hash__(self):
         return hash((self.name, self.price))
  
  
-class TooManyProductsFoundError:
+class TooManyProductsFoundError(Exception):
     # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
     pass
  
@@ -57,18 +72,34 @@ class ListServer:
         super().__init__(*args, **kwargs)
         self.products: List[Product] = products
 
-    def _get_all_products(self, letter_iter = 1) -> List[Product]:
-        return self.products
+    n_max_returned_entries = 5
+
+    def get_all_products(self, n_letters = 1) -> List[Product]:
+        selected_products = []
+        for article in self.products:
+            if article.n_letters == n_letters:
+                selected_products.append(article)
+        if len(selected_products) > self.n_max_returned_entries:
+            raise TooManyProductsFoundError
+        return selected_products
 
 
 class MapServer:
     def __init__(self, products: List[Product], *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.products: Dict[str, Product] = {p.name: p.price for p in products}
+        self.products: Dict[str, Product] = {p.name: p for p in products}
 
-    def _get_all_products(self, letter_iter = 1) -> List[Product]:
-        return list(self.products.values())
+    n_max_returned_entries = 5
 
+    def get_all_products(self, n_letters = 1) -> List[Product]:
+        selected_products = []
+        for article in self.products.values():
+            if article.n_letters == n_letters:
+                selected_products.append(article)
+        if len(selected_products) > self.n_max_returned_entries:
+            raise TooManyProductsFoundError
+        return selected_products
+ 
  
 class Client:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą obiekt reprezentujący serwer
--- servers_tests.py
+++ servers_tests.py
@@ -1,15 +1,30 @@
 import unittest
-from collections import Counter
- 
-from servers_skeleton import ListServer, Product, Client, MapServer
- 
-server_types = (ListServer, MapServer)
+
+from servers_skeleton import ListServer, Product, Client, MapServer, TooManyProductsFoundError
 
 
 class ProductTest(unittest.TestCase):
 
     def test_is_not_none(self):
         self.assertIsNotNone(Product('D12', 19))
+
+    def test_product_works_properly_1(self):
+        product = Product('D12', 19)
+        self.assertEqual(product.n_letters, 1)
+        self.assertEqual(product.price, 19)
+        self.assertEqual(product.name, 'D12')
+
+    def test_product_works_properly_2(self):
+        product = Product('AAAAAA123456789', 40)
+        self.assertEqual(product.n_letters, 6)
+        self.assertEqual(product.price, 40)
+        self.assertEqual(product.name, 'AAAAAA123456789')
+
+    def test_product_works_properly_3(self):
+        product = Product('DsPr1224', 124)
+        self.assertEqual(product.n_letters, 4)
+        self.assertEqual(product.price, 124)
+        self.assertEqual(product.name, 'DsPr1224')
 
 
 class ProductValueErrorTest(unittest.TestCase):
@@ -74,24 +89,77 @@ class ProductEqualityTest(unittest.TestCase):
         product_2 = Product('S12', 9)
         self.assertNotEqual(product_1, product_2)
 
-'''
-class ServerTest(unittest.TestCase):
- 
-    def test_get_entries_returns_proper_entries(self):
-        products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1)]
-        for server_type in server_types:
-            server = server_type(products)
-            entries = server.get_entries(2)
-            self.assertEqual(Counter([products[2], products[1]]), Counter(entries))
- 
- 
-class ClientTest(unittest.TestCase):
-    def test_total_price_for_normal_execution(self):
-        products = [Product('PP234', 2), Product('PP235', 3)]
-        for server_type in server_types:
-            server = server_type(products)
-            client = Client(server)
-            self.assertEqual(5, client.get_total_price(2))'''
+
+class ListServerTest(unittest.TestCase):
+
+    def test_constructor(self):
+        product_1 = Product('A123', 1)
+        product_2 = Product('a123', 2)
+        list_server = ListServer([product_1, product_2])
+
+        self.assertEqual(list_server.products, [product_1, product_2])
+
+    def test_get_entries(self):
+        product_1 = Product('A123', 1)
+        product_2 = Product('a123', 2)
+        product_3 = Product('aA123', 3)
+        product_4 = Product('A1', 4)
+        product_5 = Product('a1', 5)
+        product_6 = Product('aA1', 6)
+        list_of_products = [product_1, product_2, product_3, product_4, product_5, product_6]
+        list_server = ListServer(list_of_products)
+
+        self.assertEqual(list_server.get_all_products(1), [product_1, product_2, product_4, product_5])
+        self.assertEqual(list_server.get_all_products(2), [product_3, product_6])
+
+    def test_too_many_products(self):
+        # TODO: Ustaw wartość atrybutu n_max_returned_entries na 5 lub mniej
+        product_1 = Product('A123', 1)
+        product_2 = Product('a123', 2)
+        product_3 = Product('B123', 3)
+        product_4 = Product('A123', 4)
+        product_5 = Product('a123', 5)
+        product_6 = Product('B123', 6)
+        list_of_products = [product_1, product_2, product_3, product_4, product_5, product_6]
+        list_server = ListServer(list_of_products)
+
+        self.failUnlessRaises(TooManyProductsFoundError, list_server.get_all_products)
+
+
+class MapServerTest(unittest.TestCase):
+
+    def test_constructor(self):
+        product_1 = Product('A123', 1)
+        product_2 = Product('a123', 2)
+        list_server = MapServer([product_1, product_2])
+
+        self.assertEqual(list_server.products, {"A123": product_1, "a123": product_2})
+
+    def test_get_entries(self):
+        product_1 = Product('A123', 1)
+        product_2 = Product('a123', 2)
+        product_3 = Product('aA123', 3)
+        product_4 = Product('A1', 4)
+        product_5 = Product('a1', 5)
+        product_6 = Product('aA1', 6)
+        list_of_products = [product_1, product_2, product_3, product_4, product_5, product_6]
+
+        map_server = MapServer(list_of_products)
+        self.assertEqual(map_server.get_all_products(1), [product_1, product_2, product_4, product_5])
+        self.assertEqual(map_server.get_all_products(2), [product_3, product_6])
+
+    def test_too_many_products(self):
+        # TODO: Ustaw wartość atrybutu n_max_returned_entries na 5 lub mniej
+        product_1 = Product('A123', 1)
+        product_2 = Product('a123', 2)
+        product_3 = Product('B123', 3)
+        product_4 = Product('A1', 4)
+        product_5 = Product('a1', 5)
+        product_6 = Product('B1', 6)
+        list_of_products = [product_1, product_2, product_3, product_4, product_5, product_6]
+        map_server = MapServer(list_of_products)
+
+        self.failUnlessRaises(TooManyProductsFoundError, map_server.get_all_products)
  
  
 if __name__ == '__main__':
